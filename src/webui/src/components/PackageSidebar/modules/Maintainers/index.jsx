9cdfca67cce4313bf027d88ca925f32f9da67346
Jonas Winzen <jwin@de.ibm.com>
2018-07-15 14:22:07 +0200

    fix: show correct number of contributors

--- src/webui/src/components/PackageSidebar/modules/Maintainers/index.jsx
+++ src/webui/src/components/PackageSidebar/modules/Maintainers/index.jsx
@@ -18,7 +18,6 @@ export default class Maintainers extends React.Component {
 
   constructor(props) {
     super(props);
-    this.handleShowAllContributors = this.handleShowAllContributors.bind(this);
   }
 
   get author() {
@@ -26,43 +25,39 @@ export default class Maintainers extends React.Component {
   }
 
   get contributors() {
-    let contributors = get(this, 'props.packageMeta.latest.contributors', {});
-    return filter(contributors, (contributor) => {
-      return (
-        contributor.name !== get(this, 'author.name') &&
-        contributor.email !== get(this, 'author.email')
-      );
-    });
+    let contributors = get(this, 'props.packageMeta.latest.contributors', []);
+    return filter(uniqBy(contributors, (contributor) => contributor.name), (contributor) => {
+      return contributor.name !== get(this, 'author.name') && contributor.email !== get(this, 'author.email');
+    }).sort(({name: l}, {name: r}) => l.localeCompare(r));
   }
 
   get showAllContributors() {
     return this.state.showAllContributors || size(this.contributors) <= 5;
   }
 
-  get uniqueContributors() {
+  get shortContributorList() {
     if (!this.contributors) {
       return [];
     }
-
-    return uniqBy(this.contributors, (contributor) => contributor.name).slice(0, 5);
+    return this.contributors.slice(0, 5);
   }
 
-  handleShowAllContributors() {
+  handleShowAllContributors = () => {
     this.setState({
       showAllContributors: true
     });
-  }
+  };
 
   renderContributors() {
     if (!this.contributors) return null;
 
-    return (this.showAllContributors ? this.contributors : this.uniqueContributors)
-      .map((contributor, index) => {
+    return (this.showAllContributors ? this.contributors : this.shortContributorList)
+      .map(({name, avatar}) => {
         return <MaintainerInfo
-          key={index}
+          key={name}
           title="Contributors"
-          name={contributor.name}
-          avatar={contributor.avatar}/>;
+          name={name}
+          avatar={avatar}/>;
       });
   }
 
@@ -70,21 +65,18 @@ export default class Maintainers extends React.Component {
     let author = this.author;
 
     return (
-      <Module
-        title="Maintainers"
-        className={classes.maintainersModule}
-      >
+      <Module title={`Maintainers (${this.contributors.length + 1})`} className={classes.maintainersModule}>
         <ul className="maintainer-author">
-          {author && <MaintainerInfo title="Author" name={author.name} avatar={author.avatar}/>}
+          {author && <MaintainerInfo title="Author" name={author.name} avatar={author.avatar} />}
           {this.renderContributors()}
         </ul>
         {!this.showAllContributors && (
           <button
             onClick={this.handleShowAllContributors}
             className={classes.showAllContributors}
-            title="Current list only show the author and first 5 contributors unique by name"
+            title={`Show all ${this.contributors.length + 1} contributors`}
           >
-            Show all contributor
+            Show all
           </button>
         )}
       </Module>
